fa5e89b807d759964876a4ecc4d7af77f3796c43
Robert Anderson <robert.anderson@kcl.ac.uk>
2022-03-18 17:10:17 +0100

    making the interface C++ compatible

--- my_interface.h
+++ my_interface.h
@@ -2,6 +2,10 @@
 /*
  * define C headers for the fortran library function and subroutine defs
  */
+#ifdef __cplusplus
+#define extern extern "C"
+#endif
+
 extern int add_func(const int* a, const int* b);
 extern void add_sub(const int* a, const int* b, int* c);
 extern void add_explicit_shape_mats(const int* a, const int* b, int* c, const int* ncol, const int* nrow);
--- test.sh
+++ test.sh
@@ -2,9 +2,13 @@
 
 FC=gfortran
 CC=gcc
+CXX=g++
 
 $FC -c -fPIC my_fortran_lib.f90 -o my_fortran_lib.o
 $FC my_fortran_lib.o -shared -o my_fortran_lib.so
 $CC -c main.c -o main.o
 $CC main.o my_fortran_lib.o -o a.out
+./a.out
+$CXX -c main.cpp -o main.o
+$CXX main.o my_fortran_lib.o -o a.out
 ./a.out
